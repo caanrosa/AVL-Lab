599006bfbdcd84528632e8c3e873912b1ed0d1ed
DevJeffreyG <gabrielpaez.r@hotmail.com>
2024-09-08 11:16:34 -0500

    comparaciones con la métrica

--- dataset.py
+++ dataset.py
@@ -3,14 +3,15 @@ import csv
 import os
 
 dirname = os.path.dirname(__file__)
-file_path = dirname + "/dataset/dataset_movies.csv"
-tree = AVL()
+file_path = dirname + "/dataset/short_dataset.csv" # uno con menos para poder VISUALIZARLO
+tree = AVL("Title")
 
 
 with open(file_path, mode='r') as file:
     lector_csv = csv.DictReader(file)
     for fila in lector_csv:
-        titulo = fila['Title']
-        datos = dict(fila)  # Convertir la fila en un diccionario
+        #titulo = fila['Title']
+        #datos = dict(fila)  # Convertir la fila en un diccionario
         tree.insert(fila)
 
+tree.graph("datasetCompleto").view()
